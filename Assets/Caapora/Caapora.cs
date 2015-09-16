c08950182d5ddce29130dd46cfd078240c5cbaa9
Mateus Souza da Silva <mma.teuzzz@gmail.com>
2015-09-16 15:23:55 -0300

    Adição do Player, Animations e AnimationController.

--- Assets/Caapora/Caapora.cs
+++ Assets/Caapora/Caapora.cs
@@ -4,38 +4,49 @@ using IsoTools;
 
 public class Caapora : MonoBehaviour {
 
-	// public GameObject gameObject = null;
+    // public GameObject gameObject = null;
+    public Animator animator;
 
 	// Use this for initialization
 	void Start () {
 
-		// gameObject = Instantiate(Resources.Load("Prefabs/FloorPrefab")) as GameObject;
-	
-	}
+        animator = GetComponent<Animator>();
+        // gameObject = Instantiate(Resources.Load("Prefabs/FloorPrefab")) as GameObject;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
 
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
 		
 			gameObject.GetComponent<IsoObject>().position += new Vector3(-0.1f , 0, 0);
-		}
+            animator.SetTrigger("CaaporaWest");
 
-		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+		} 
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
 			
 			gameObject.GetComponent<IsoObject>().position += new Vector3(0.1f , 0, 0);
-		}
+            animator.SetTrigger("CaaporaEast");
 
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        } 
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
 			
 			gameObject.GetComponent<IsoObject>().position += new Vector3(0 , -0.1f, 0);
-		}
+            animator.SetTrigger("CaaporaSouth");
 
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
 			
 			gameObject.GetComponent<IsoObject>().position += new Vector3(0 , 0.1f, 0);
-		}
+            animator.SetTrigger("CaaporaNorth");
+
+        }
+        if (!Input.GetKeyDown(KeyCode.UpArrow) || !Input.GetKeyDown(KeyCode.DownArrow) || !Input.GetKeyDown(KeyCode.RightArrow) || !Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetTrigger("CaaporaIdle");
+        }
 
 	
 	}
