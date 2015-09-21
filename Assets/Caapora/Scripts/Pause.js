4831262b82d53fe2fd824c05e5fed2c602845a88
Delboyss <danyel.speedy@gmail.com>
2015-09-21 13:01:28 -0300

    Inserção de som no gameplay e tentativa de implantar o pause

---
+++ Assets/Caapora/Scripts/Pause.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var controlePause : boolean;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if(Input.GetKeyDown(KeyCode.P)){
+		
+		if(controlePause){
+			Time.timeScale = 0;
+			controlePause = false;
+		}
+		else{
+			Time.timeScale = 1;
+			controlePause = true;
+		}
+	}
+}
