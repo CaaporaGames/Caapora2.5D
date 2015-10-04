d1cb0e0858c7c1aef9918a3a10cf5ff62d95da2f
Rômulo Lima <romulolink@gmail.com>
2015-10-04 11:02:07 -0300

    Mapa gerado por codigo

--- Assets/generator.cs
+++ Assets/generator.cs
@@ -8,8 +8,12 @@ public class generator : MonoBehaviour {
 	
 		float cellY;
 		float cellX;
-		int tileSize = 16;
+		int mapize = 50;
+		int tile_width = 34;
+		int tile_height = 16;
 
+
+		/*
 		for (cellX = 0; cellX < 30; cellX++){
 			for (cellY = 0; cellY < 30; cellY++) {
 			
@@ -17,11 +21,32 @@ public class generator : MonoBehaviour {
 				GameObject myFloorInstance = Instantiate(Resources.Load("Prefabs/grassPrefab"),
 				    new Vector3(( cellX * tileSize / 2) + (cellY * tileSize / 2), 
 				            	( cellY * tileSize / 2) - (cellX * tileSize / 2), 2),
-				                                         Quaternion.identity) as GameObject; 
+				                         Quaternion.identity) as GameObject; 
 
 
 				}	
+		} */
+
+
+		for (int i = 0; i < mapize; i++) {
+		
+			for (int j = mapize; j >= 0; j--){
+				// Changed loop condition here.
+
+				GameObject myFloorInstance = Instantiate(Resources.Load("Prefabs/grassPrefab"),
+				           new Vector3(( j * tile_width / 2) + (i * tile_width / 2), 
+				            ( i * tile_height / 2) - (j * tile_height / 2), 2),
+				
+				                                         Quaternion.identity) as GameObject; 
+				/*draw(
+					tile_map[i][j],
+					x = (j * tile_width / 2) + (i * tile_width / 2)
+					y = (i * tile_height / 2) - (j * tile_height / 2)
+					)*/
+
+			}  
 		}
+			
 				
 					
 	}
